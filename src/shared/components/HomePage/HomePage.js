6e40e846ae839999377797450fc1ebbc5d7a7c5f
Hugh Loughrey <hugh.loughrey@purplebricks.com>
2018-11-18 11:15:31 +0000

    Remove FAQ section

--- src/shared/components/HomePage/HomePage.js
+++ src/shared/components/HomePage/HomePage.js
@@ -7,22 +7,20 @@ import ContactUs from '../ContactUs/ContactUs';
 import SchoolsGroups from '../SchoolsGroups/SchoolsGroups';
 import PhotoSlideConversions from '../PhotoSlideConversions/PhotoSlideConversions';
 import VideoTapeConversions from '../VideoTapeConversions/VideoTapeConversions';
-import FAQ from '../FAQ/FAQ';
 
 // Import Text
 import text from '../../lib/text';
 
 const HomePage = () => (
-	<div className='row'>
-		<Hero />
-		<About />
-		<Weddings />
-		<SchoolsGroups />
-		<VideoTapeConversions />
-		<PhotoSlideConversions />
-		<FAQ text={text.FAQ}/>
-		<ContactUs />
-	</div>
+    <div className="row">
+        <Hero />
+        <About />
+        <Weddings />
+        <SchoolsGroups />
+        <VideoTapeConversions />
+        <PhotoSlideConversions />
+        <ContactUs />
+    </div>
 );
 
 export default HomePage;
